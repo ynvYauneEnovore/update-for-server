b56597ee360312a8ed42d0944e378c52eb33c4a6
ynvYauneEnovore <yovanuxf@gmail.com>
2023-09-05 17:28:31 -0400

    traslte

--- resources/js/Pages/Portafolio.vue
+++ resources/js/Pages/Portafolio.vue
@@ -12,34 +12,26 @@ defineProps({
 <template>
     <Head title="Hi" />
     <!-- component -->
-    <div class="py-16 bg-white">
+    <div class="bg-white">
         <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
             <div
                 class="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12"
             >
                 <div class="md:5/12 lg:w-5/12">
                     <img
-                        src="https://secure.gravatar.com/avatar/307f127a06b20c136e863f585a41f548?s=800&d=identicon"
+                        src="/img/1.png"
                         alt="image"
                         loading="lazy"
-                        width="400"
+                        width="350"
                         height=""
                     />
                 </div>
                 <div class="md:7/12 lg:w-6/12">
                     <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">
-                        Amante de la tecnología 💻
+                        Technology enthusiast 💻
                     </h2>
                     <p class="mt-6 text-gray-600">
-                        Soy Yovan, un apasionado desarrollador Full Stack con un
-                        enfoque sólido en el desarrollo backend. Tengo una
-                        amplia experiencia en la creación de aplicaciones web
-                        robustas y escalables desde cero. Mi habilidad principal
-                        radica en diseñar y desarrollar las lógicas y bases de
-                        datos que hacen funcionar las aplicaciones, garantizando
-                        un rendimiento óptimo y una seguridad sólida. Además,
-                        tengo un firme compromiso con la excelencia y la mejora
-                        continua en el campo del desarrollo web.
+I'm Yovan, a passionate Full Stack developer with a strong focus on backend development. I have extensive experience building robust and scalable web applications from scratch. My main experience lies in designing and developing the logic and databases that drive applications, ensuring optimal performance and strong security. Furthermore, I am deeply committed to excellence and continuous improvement in the field of information technology.
                     </p>
                 </div>
             </div>
