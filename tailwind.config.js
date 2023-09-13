e0c9b870d3a7dd5de549a8ed79ae77927c715ef6
ynvYauneEnovore <yovanuxf@gmail.com>
2023-09-13 12:59:35 -0400

    nueva interdacesdsd

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        './node_modules/flowbite/**/*.js',
+
     ],
 
     theme: {
@@ -20,5 +22,7 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [forms, typography, require('flowbite/plugin')
+],
+
 };
