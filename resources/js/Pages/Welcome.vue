497e72aa64be35b36d0804755ea2963d9a81cf5d
ynvYauneEnovore <yovanuxf@gmail.com>
2023-09-13 21:22:07 -0400

    nuvo modleosdssd

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -169,7 +169,7 @@ defineProps({
                 <img alt="profil" src="/img/ynv.png" class="mx-auto object-cover h-40 w-40" />
             </a>
         </div> -->
-            <div class="text-center">
+            <div class="text-center mt-5">
                 <p class="text-2xl text-gray-800 dark:text-white">
                     Yovan Enovore
                 </p>
@@ -234,6 +234,14 @@ defineProps({
         
     </div>
 
+                        <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+
+
+                            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a
+                                    href="https://yovanenovore.tech/" class="hover:underline ">YovanEnovore ™</a> <span
+                                    class="audio-element">Todos los Derechos Reservados.</span></span>
+                        </div>
 
 </template>
 
